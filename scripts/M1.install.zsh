c8a6d3238049f553e61439d37e7b9da5970b05b9
Matteo Conti <contimatteo1505@gmail.com>
2022-01-17 18:09:41 +0100

    M1 `requirements` + vscode `settings`

---
+++ scripts/M1.install.zsh
@@ -0,0 +1,12 @@
+#
+
+
+pip install --no-binary=h5py h5py
+
+
+export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1
+export GRPC_PYTHON_BUILD_SYSTEM_ZLIB=1   
+pip install grpcio
+
+
+pip install -r requirements.M1.txt
--- src/train.py
+++ src/train.py
@@ -2,9 +2,10 @@
 
 
 def main():
-  pass
+    pass
+
 
 #
 
 if __name__ == '__main__':
-  main()
+    main()
