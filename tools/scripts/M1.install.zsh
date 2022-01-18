8f642a8f9fb285f3259789509ce6d23e4e2a0eb7
Matteo Conti <contimatteo1505@gmail.com>
2022-01-18 22:27:23 +0100

    ISSUE persisting on installing `pandas` lib on Apple M1 (silicon).

--- tools/scripts/M1.install.zsh
+++ tools/scripts/M1.install.zsh
@@ -9,9 +9,9 @@ pip install grpcio
 
 
 pip install cython
-# pip install --no-binary :all: --no-use-pep517 pandas
-pip install --no-binary :all: pandas
+pip install --no-binary :all: --no-use-pep517 pandas
+# pip install --no-binary :all: pandas
 
 
-# pip install --no-cache-dir -r requirements.M1.txt 
 pip install -r requirements.M1.txt 
+# pip install --no-cache-dir -r requirements.M1.txt 
