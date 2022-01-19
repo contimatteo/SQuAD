182f7f39b2d1554a6218acd52d87436a5ab8c72c
Matteo Conti <contimatteo1505@gmail.com>
2022-01-19 11:17:42 +0100

    `pandas` Apple M1 ISSUE fixed.

--- tools/scripts/M1.install.zsh
+++ tools/scripts/M1.install.zsh
@@ -9,9 +9,7 @@ pip install grpcio
 
 
 pip install cython
-pip install --no-binary :all: --no-use-pep517 pandas
-# pip install --no-binary :all: pandas
+pip install git+git://github.com/pandas-dev/pandas.git
 
 
 pip install -r requirements.M1.txt 
-# pip install --no-cache-dir -r requirements.M1.txt 
