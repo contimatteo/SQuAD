c9ca22f023715a647af18a1066d6a09f1c7dc77a
primianocristino <primi.armi@libero.it>
2022-02-07 12:21:43 +0100

    evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,8 +2,10 @@ import os
 from utils.data import get_project_directory
 
 
-def evaluate(data_path: str, prediction_path: str, na_prob_path=None):
-    command = f'{os.path.join("utils", "evaluate.py")} "{str(data_path)}" "{str(prediction_path)}" '
+def evaluate(data_path: str, prediction_path: str, output_file=None, na_prob_path=None):
+    command = f'{os.path.join("utils", "evaluate.py")} "{str(data_path)}" "{str(prediction_path)}"'
+    if output_file is not None:
+        command = command + ' -o "' + output_file + '"'
     if na_prob_path is not None:
         command = command + ' -n "' + na_prob_path + '"'
     print(command)
@@ -15,4 +17,4 @@ def evaluate(data_path: str, prediction_path: str, na_prob_path=None):
 if __name__ == "__main__":
     eval_folder = os.path.join(os.path.join(get_project_directory(), "data"), "evaluation")
     print(eval_folder)
-    evaluate(os.path.join(eval_folder, "data.json"), os.path.join(eval_folder, "preds.json"))
+    evaluate(os.path.join(eval_folder, "data.json"), os.path.join(eval_folder, "preds.json"), output_file=os.path.join(eval_folder, "eval.json"))
