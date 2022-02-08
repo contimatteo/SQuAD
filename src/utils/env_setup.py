e656ebfbe3b6dc896df77acb5e178f06cd7bde93
Matteo Conti <contimatteo1505@gmail.com>
2022-02-08 17:17:20 +0100

    minor changes

--- src/utils/env_setup.py
+++ src/utils/env_setup.py
@@ -31,3 +31,11 @@ else:
 import tensorflow as tf
 
 tf.config.run_functions_eagerly(True)
+
+# gpus = tf.config.list_physical_devices('GPU')
+# if gpus:
+#     try:
+#         for gpu in gpus:
+#             tf.config.experimental.set_memory_growth(gpu, True)
+#     except RuntimeError as e:
+#         pass
