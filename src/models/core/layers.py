226fda3365b73bf52617d25400acdcb7673c677f
primianocristino <primi.armi@libero.it>
2022-02-25 11:52:31 +0100

    3lstm

--- src/models/core/layers.py
+++ src/models/core/layers.py
@@ -67,19 +67,17 @@ def DrqaRnn() -> Callable[[Any], Any]:
 
     def _nn(x: Any) -> Any:
         x = rnn1(x)
-        # x = rnn2(x)
-        # x = rnn3(x)
+        x = rnn2(x)
+        x = rnn3(x)
         return x
 
     return _nn
 
 
 def Mask_layer():
-
-    def _nn(tensor: Any, mask: Any) -> Any:
+    def _nn(rnn: Any, mask: Any) -> Any:
         mask_expanded = expand_dims(mask, axis=2)
-        return mask_expanded * tensor
-
+        return mask_expanded * rnn
     return _nn
 
 
